8df0a6f0828c82bcf4940cb3681f93a1d72920a6
Vincent Petry <PVince81@yahoo.fr>
2012-07-13 21:22:56 +0200

    Added rendering code

--- main.lua
+++ main.lua
@@ -1,9 +1,39 @@
 require "model"
 
+FIELD_SIZE = 20
+
+function love.conf(t)
+    t.screen.width = 800
+    t.screen.height = 600
+end
+
 function love.load()
     level = loadLevel(0)
+    player = createPlayer()
+
+    player.x = level.playerStartX
+    player.y = level.playerStartY
 end
 
 function love.draw()
-    
+    love.graphics.setColor(255, 255, 255, 255)
+
+    -- render map
+    offsetY = 0
+    for y=0,level.height do
+        row = level.fields[y]
+        offsetX = 0
+        for x=0,level.width do
+            field = row[x]
+            if field == FIELD_BLOCK then
+                love.graphics.rectangle("fill", offsetX, offsetY, FIELD_SIZE, FIELD_SIZE)
+            end
+            offsetX = offsetX + FIELD_SIZE
+        end
+        offsetY = offsetY + FIELD_SIZE
+    end
+
+    love.graphics.setColor(128, 0, 0, 255)
+    -- render player
+    love.graphics.circle("fill", (player.x  - 0.5) * FIELD_SIZE, (player.y - 0.5) * FIELD_SIZE, FIELD_SIZE / 2.0)
 end
--- model.lua
+++ model.lua
@@ -6,8 +6,8 @@ FIELD_LADDER = 2
 function loadLevel(num)
     level = {}
 
-    width = 50
-    height = 50
+    width = 20
+    height = 20
 
     fields = {}
     for y=0,height do
@@ -20,16 +20,26 @@ function loadLevel(num)
 
     for x=0,width do
         fields[height-1][x] = FIELD_BLOCK
-        fields[height-3][x] = FIELD_BLOCK
+        fields[height-4][x] = FIELD_BLOCK
     end
 
     fields[height-1][width - 1] = FIELD_LADDER
     fields[height-2][width - 1] = FIELD_LADDER
     fields[height-3][width - 1] = FIELD_LADDER
+    fields[height-5][width - 1] = FIELD_LADDER
 
     
-    level["width"] = width
-    level["height"] = height
-    level["fields"] = fields
+    level.width = width
+    level.height = height
+    level.fields = fields
+    level.playerStartX = 1
+    level.playerStartY = height - 1
     return level
+end
+
+function createPlayer()
+    player = {}
+    player.x = 0
+    player.y = 0
+    return player
 end
