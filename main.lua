e0c9916dccba5981e68d9a4bc869a3b5376686a7
Vincent Petry <PVince81@yahoo.fr>
2012-07-14 08:31:40 +0200

    Added Tiled map loader

--- main.lua
+++ main.lua
@@ -3,7 +3,8 @@ require "model"
 FIELD_SIZE = 25
 
 level = nil
-player = nil
+player = {x=0, y=0}
+cam = {x=0, y=0}
 sprites = nil
 MOMENTUM = 0
 
@@ -23,19 +24,30 @@ function loadSprites()
 end
 
 function love.load()
-    loadSprites()
-    
-    level = loadLevel(0)
-    player = createPlayer()
 
-    player.x = level.playerStartX
-    player.y = level.playerStartY
+    TiledMap_Load("map/map1.tmx", FIELD_SIZE)
+    player.x = 1
+    player.y = 12
+    cam.x = player.x * FIELD_SIZE
+    cam.y = player.y * FIELD_SIZE
+    -- TODO: use sprites from tiled map?
+    loadSprites()
+
+--    level = loadLevel(1)
+    --saveLevel(1, level)
+    
+    --level = loadLevelOld(1)
+--    player = createPlayer()
+
+    --player.x = level.playerStartX
+    --player.y = level.playerStartY
 end
 
 function love.update(dt)
    if love.keyboard.isDown("right") then
       MOMENTUM = MOMENTUM + 0.5 * dt
       player.x = player.x + MOMENTUM * dt
+      cam.x = cam.x + MOMENTUM * dt * FIELD_SIZE
    end
 end
 
@@ -46,31 +58,36 @@ end
 function love.draw()
     love.graphics.setColor(255, 255, 255, 255)
 
-    -- render map
-    offsetY = 0
-    for y=0,level.height do
-        row = level.fields[y]
-        offsetX = 0
-        for x=0,level.width do
-            field = row[x]
-            if field == FIELD_BLOCK then
-                love.graphics.drawq(spritesImage, sprites.block, offsetX, offsetY)
-            elseif field == FIELD_LADDER then
-                love.graphics.drawq(spritesImage, sprites.ladder, offsetX, offsetY)
-            end
-            offsetX = offsetX + FIELD_SIZE
-        end
-        offsetY = offsetY + FIELD_SIZE
-    end
+    TiledMap_DrawNearCam(cam.x,cam.y)
 
-    -- render player
+    -- render map
+--     offsetY = 0
+--     for y=0,level.height do
+--         row = level.fields[y]
+--         offsetX = 0
+--         for x=0,level.width do
+--             field = row[x]
+--             if field == FIELD_BLOCK then
+--                 love.graphics.drawq(spritesImage, sprites.block, offsetX, offsetY)
+--             elseif field == FIELD_LADDER then
+--                 love.graphics.drawq(spritesImage, sprites.ladder, offsetX, offsetY)
+--             end
+--             offsetX = offsetX + FIELD_SIZE
+--         end
+--         offsetY = offsetY + FIELD_SIZE
+--     end
+-- 
+    -- render player    
+    mapOffsetX = love.graphics.getWidth() / 2 - cam.x
+    mapOffsetY = love.graphics.getHeight() / 2 - cam.y
+    
     if MOMENTUM < 0 then
         playerSprite = sprites.playerLeft
     else
         playerSprite = sprites.playerRight
     end
 
-    offsetX = (player.x  - 0.5) * FIELD_SIZE
-    offsetY = player.y  * FIELD_SIZE - FIELD_SIZE
+    offsetX = mapOffsetX + (player.x  - 0.5) * FIELD_SIZE
+    offsetY = mapOffsetY + player.y  * FIELD_SIZE - FIELD_SIZE
     love.graphics.drawq(spritesImage, playerSprite, offsetX, offsetY)
 end
--- model.lua
+++ model.lua
@@ -1,9 +1,54 @@
+love.filesystem.load("lib/tiledmap.lua")()
+
 FIELD_EMPTY = 0
 FIELD_BLOCK = 1
 FIELD_LADDER = 2
 
 
 function loadLevel(num)
+    local filename = "level" .. num .. ".dat"
+    local data = love.filesystem.lines( filename )
+    print(data[0])
+    for line in data do
+        print(line)
+    end
+    --width = result[0]
+    --height = result[1]
+    --for i=2, # result do
+        --row = result[i]
+    --end
+    --print(width, height)
+    
+end
+
+function saveLevel(num, level)
+    local filename = "level" .. tostring(num) .. ".dat"
+
+    str = ""
+
+    str = str .. tostring(level.width) .. "\n"
+    str = str .. tostring(level.height) .. "\n"
+    
+    for y=0,height do
+        row = level.fields[y]
+        for x=0,width do
+            field = row[x]
+            c = "_"
+            if field == FIELD_BLOCK then
+                c = "X"
+            elseif field == FIELD_LADDER then
+                c = "#"
+            end
+            str = str .. c
+        end
+        str = str .. "\n"
+    end
+        
+    --love.filesystem.save( filename )
+    print(str)
+end
+
+function loadLevelOld(num)
     level = {}
 
     width = 20
